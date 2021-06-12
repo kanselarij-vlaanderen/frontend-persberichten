08d2b302834621be26a2f2ab6312d4bdd7a3877a
Erika Pauwels <erika.pauwels@gmail.com>
2021-06-12 17:53:29 +0200

    Remove unused code

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,5 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
 export default class ApplicationController extends Controller {
 
-  @action
-  showSettingsModal() {
-    //todo
-    console.log('modal opens');
-  }
 }
