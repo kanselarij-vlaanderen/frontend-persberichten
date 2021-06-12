928ab279da8056739db5879048c52c9eafe55de8
Erika Pauwels <erika.pauwels@gmail.com>
2021-06-12 12:24:28 +0200

    Refactor page title component to auk-components

--- app/controllers/sources/overview.js
+++ app/controllers/sources/overview.js
@@ -1,9 +1,5 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 
 export default class SourcesOverviewController extends Controller {
-  @action
-  navigateToNewSource() {
-    this.transitionToRoute('sources.new');
-  }
+
 }
