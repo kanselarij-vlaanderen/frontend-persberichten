efc8355e8beb0524ccae435c91374cb8a094237b
Michael Vervloet <michael.vervloet@ordina.be>
2021-07-27 11:46:44 +0200

    fixes feedback

--- app/models/government-field.js
+++ app/models/government-field.js
@@ -2,5 +2,5 @@ import Model, { attr, hasOne } from '@ember-data/model';
 
 export default class GovernmentFieldModel extends Model {
   @attr('string') prefLabel;
-  @hasOne('has-government-domain') hasGovernmentDomain;
+  @hasOne('government-domain') governmentDomain;
 }
