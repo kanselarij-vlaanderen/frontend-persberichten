ea35b3bc3cd5b24d35d10be56a2f9db58d82483f
Eduard Tsatourov <eduard.tsatourov@ordina.be>
2021-07-29 10:52:30 +0200

    fixes code review feedback

--- app/routes/press-releases/press-release.js
+++ app/routes/press-releases/press-release.js
@@ -3,7 +3,11 @@ import PressReleaseSnapshot from '../../utils/press-release-snapshot';
 
 export default class PressReleasesPressReleaseRoute extends Route {
   async model(params) {
-    const pressRelease = await this.store.findRecord('press-release', params.press_release_id);
+    const pressRelease = await this.store.findRecord('press-release', params.press_release_id, {
+      include: [
+        'publication-event'
+      ].join(',')
+    });
     // include relations when more fields are added => e.g. sources.js route
 
     const snapshot = new PressReleaseSnapshot(pressRelease);
