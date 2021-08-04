a87967e1292c4c40deb45148a4cbee582081f5f9
Michael Vervloet <michael.vervloet@ordina.be>
2021-08-04 09:53:24 +0200

    renamed field

--- app/models/government-field.js
+++ app/models/government-field.js
@@ -2,5 +2,5 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class GovernmentFieldModel extends Model {
   @attr('string') prefLabel;
-  @belongsTo('government-domain') domain;
+  @belongsTo('government-domain') governmentDomain;
 }
