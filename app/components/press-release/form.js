03da95bbe61270f521b49bf290ffd3bfb429dfb1
Eduard Tsatourov <eduard.tsatourov@ordina.be>
2021-08-06 02:49:36 +0200

    adds keywords to pressrelease form

--- app/components/press-release/form.js
+++ app/components/press-release/form.js
@@ -6,4 +6,26 @@ export default class PressReleaseFormComponent extends Component {
   setInputValue(record, attribute, event) {
     record[attribute] = event.target.value;
   }
+
+  @action
+  addKeyword(model, newKeyword) {
+    let keyWordArray = model.keyword;
+    if (keyWordArray) {
+      keyWordArray.push(newKeyword)
+      model.keyword = [...new Set(keyWordArray)];
+    } else {
+      keyWordArray = new Set();
+      keyWordArray.add(newKeyword)
+      let newKeywordArray = [...keyWordArray]
+      model.keyword = newKeywordArray;
+    }
+  }
+
+  @action
+  deleteKeyword(model, keyword) {
+    const keyWordArray = model.keyword;
+    const index = keyWordArray.indexOf(keyword);
+    keyWordArray.splice(index, 1);
+    model.keyword = [...keyWordArray];
+  }
 }
