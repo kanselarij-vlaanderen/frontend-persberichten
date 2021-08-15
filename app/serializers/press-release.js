b9ccc932d497acbacec9e28ee6183d722f357619
Eduard Tsatourov <eduard.tsatourov@ordina.be>
2021-08-15 23:44:51 +0200

    fixes serializer

--- app/serializers/press-release.js
+++ app/serializers/press-release.js
@@ -2,7 +2,7 @@ import ApplicationSerializer from './application';
 
 export default class PressReleaseSerializer extends ApplicationSerializer {
   shouldSerializeHasMany(snapshot, key/*, relationshipType*/) {
-    if (key == 'publicationChannels' || 'contact')
+    if (key == 'publicationChannels' || 'sources')
       return true;
     else
       return super.shouldSerializeHasMany(...arguments);
