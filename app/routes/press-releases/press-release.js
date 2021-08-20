11d28dbabe394d36d750e4efeab52ee7b212d8d1
Eduard Tsatourov <eduard.tsatourov@ordina.be>
2021-08-21 01:47:07 +0200

    cleans code

--- app/routes/press-releases/press-release.js
+++ app/routes/press-releases/press-release.js
@@ -2,14 +2,6 @@ import Route from '@ember/routing/route';
 import PressReleaseSnapshot from '../../utils/press-release-snapshot';
 
 export default class PressReleasesPressReleaseRoute extends Route {
-  // beforeModel(transition) {
-  //   if(transition.from) {
-  //     this.fromRoute = transition.from.name;
-  //   } else {
-  //     this.fromRoute = 'press-releases.overview.concept';
-  //   }
-  // }
-
   async model(params) {
     const pressRelease = await this.store.findRecord('press-release', params.press_release_id, {
       include: [
@@ -21,9 +13,4 @@ export default class PressReleasesPressReleaseRoute extends Route {
     await snapshot.commit();
     return snapshot;
   }
-
-  // setupController(controller, model) {
-  //   super.setupController(controller, model);
-  //   controller.fromRoute = this.fromRoute;
-  // }
 }
--- app/routes/press-releases/press-release/published.js
+++ app/routes/press-releases/press-release/published.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class PressReleasesPressReleasePublishedRoute extends Route {
   setupController(controller, model) {
-    console.log(model)
     super.setupController(controller, model);
   }
 }
