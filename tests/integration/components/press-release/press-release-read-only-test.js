ad5b1b7762b61155b7551c7ac5599a4e01668520
Eduard Tsatourov <eduard.tsatourov@ordina.be>
2021-08-23 01:05:20 +0200

    changes read only file name

--- tests/integration/components/press-release/press-release-read-only-test.js
+++ tests/integration/components/press-release/press-release-read-only-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | press-release/form-published', function(hooks) {
+module('Integration | Component | press-release/press-release-read-only', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<PressRelease::FormPublished />`);
+    await render(hbs`<PressRelease::PressReleaseReadOnly />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <PressRelease::FormPublished>
+      <PressRelease::PressReleaseReadOnly>
         template block text
-      </PressRelease::FormPublished>
+      </PressRelease::PressReleaseReadOnly>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
