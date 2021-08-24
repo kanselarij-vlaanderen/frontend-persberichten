c2b3ff753b6923b4b13ce9b2bb750c3c68bc0d2b
Eduard Tsatourov <eduard.tsatourov@ordina.be>
2021-08-25 00:23:18 +0200

    fixes comments

--- app/routes/press-releases/press-release.js
+++ app/routes/press-releases/press-release.js
@@ -1,17 +1,12 @@
 import Route from '@ember/routing/route';
-import PressReleaseSnapshot from '../../utils/press-release-snapshot';
 
 export default class PressReleasesPressReleaseRoute extends Route {
-  async model(params) {
-    // const pressRelease =
-    return await this.store.findRecord('press-release', params.press_release_id, {
+  model(params) {
+    return this.store.findRecord('press-release', params.press_release_id, {
       include: [
         'publication-channels',
         'publication-event'
       ].join(',')
     });
-    const snapshot = new PressReleaseSnapshot(pressRelease);
-    await snapshot.commit();
-    return snapshot;
   }
 }
--- app/routes/press-releases/press-release/edit.js
+++ app/routes/press-releases/press-release/edit.js
@@ -3,14 +3,14 @@ import PressReleaseSnapshot from '../../../utils/press-release-snapshot';
 
 export default class PressReleasesPressReleaseEditRoute extends Route {
   async beforeModel() {
-    const isPublished = (await this.modelFor('press-releases.press-release')).publicationEvent.get('isPublished');
-    if (isPublished) {
+    const publicationEvent = await this.modelFor('press-releases.press-release').publicationEvent;
+    if (publicationEvent && publicationEvent.isPublished) {
       this.transitionTo('press-releases.press-release.published');
     }
   }
 
   async model() {
-    const pressRelease = await this.modelFor('press-releases.press-release');
+    const pressRelease = this.modelFor('press-releases.press-release');
     const snapshot = new PressReleaseSnapshot(pressRelease);
     await snapshot.commit();
     return snapshot;
