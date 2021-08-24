5c20a4fbcdf9080442c7f74e0b366a867bc7b7d7
Eduard Tsatourov <eduard.tsatourov@ordina.be>
2021-08-24 14:29:14 +0200

    fixes review comments

--- app/routes/press-releases/press-release/published.js
+++ app/routes/press-releases/press-release/published.js
@@ -1,8 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default class PressReleasesPressReleasePublishedRoute extends Route {
-  setupController(controller, model) {
-    const pressRelease = model.pressRelease;
-    super.setupController(controller, pressRelease);
-  }
 }
--- tests/integration/components/press-release/press-release-read-only-test.js
+++ tests/integration/components/press-release/press-release-read-only-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | press-release/press-release-read-only', function(hooks) {
+module('Integration | Component | press-release/read-only', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<PressRelease::PressReleaseReadOnly />`);
+    await render(hbs`<PressRelease::ReadOnly />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <PressRelease::PressReleaseReadOnly>
+      <PressRelease::ReadOnly>
         template block text
-      </PressRelease::PressReleaseReadOnly>
+      </PressRelease::ReadOnly>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
