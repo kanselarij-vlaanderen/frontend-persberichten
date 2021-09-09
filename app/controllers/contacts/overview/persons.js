748fa761ec51f8c37e535fb28cb25d96c04a9f13
Eduard Tsatourov <eduard.tsatourov@ordina.be>
2021-09-09 12:53:24 +0200

    adds sortable table to contacts page with editing option

--- app/controllers/contacts/overview/persons.js
+++ app/controllers/contacts/overview/persons.js
@@ -3,11 +3,12 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class ContactsOverviewPersonsController extends Controller {
+  @tracked selectedContact;
+  @tracked showContactItemModal = false;
+
   @tracked sort = '-full-name';
   @tracked page = 0;
   @tracked size = 25;
-
-  tempContact = {};
 
   @action
   prevPage() {
@@ -30,5 +31,52 @@ export default class ContactsOverviewPersonsController extends Controller {
   @action
   setSort(sort) {
     this.sort = sort;
+  }
+
+  @action
+  openContactItem(contact) {
+    this.selectedContact = contact;
+    this.showContactItemModal = true;
+  }
+
+  @action
+  async deleteContact() {
+    await this.selectedContact.destroyRecord();
+    this.send('reloadModel');
+    this.closeContactItemModal();
+  }
+
+  @action
+  async changeContact() {
+    const telephone = await this.selectedContact.telephone;
+    const mailAddress = await this.selectedContact.mailAddress;
+    await Promise.all([
+      telephone.hasDirtyAttributes ? await telephone.save() : null,
+      mailAddress.hasDirtyAttributes ? await mailAddress.save() : null
+    ]);
+    this.selectedContact.modified = new Date();
+    await this.selectedContact.save();
+    this.closeContactItemModal();
+  }
+
+  @action
+  async closeContactItemModalAndReset() {
+    if (this.selectedContact.hasDirtyAttributes) {
+      this.selectedContact.rollbackAttributes();
+    }
+    const telephone = await this.selectedContact.telephone;
+    const mailAddress = await this.selectedContact.mailAddress;
+    if (telephone.hasDirtyAttributes) {
+      telephone.rollbackAttributes();
+    }
+    if (mailAddress.hasDirtyAttributes) {
+      mailAddress.rollbackAttributes();
+    }
+    this.closeContactItemModal();
+  }
+
+  @action
+  closeContactItemModal() {
+    this.showContactItemModal = false;
   }
 }
--- app/routes/contacts/overview/persons.js
+++ app/routes/contacts/overview/persons.js
@@ -1,7 +1,13 @@
 import Route from '@ember/routing/route';
 // eslint-disable-next-line ember/no-mixins
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
+import { action } from '@ember/object';
 
 export default class ContactsOverviewPersonsRoute extends Route.extend(DataTableRouteMixin) {
   modelName = 'contact-item';
+
+  @action
+  reloadModel() {
+    this.refresh();
+  }
 }
