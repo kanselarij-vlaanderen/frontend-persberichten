ab419b3a4857363ece8994e885f7283c8dc670b0
Eduard Tsatourov <eduard.tsatourov@ordina.be>
2021-09-24 16:54:03 +0200

    for debugging

--- app/controllers/press-releases/press-release/shared/read.js
+++ app/controllers/press-releases/press-release/shared/read.js
@@ -1,9 +1,41 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency-decorators';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class PressReleasesPressReleaseSharedReadController extends Controller {
   @service currentSession;
 
+  @tracked collaboration;
   @tracked collaborators;
+  @tracked showApprovalModal = false;
+
+  @task
+  *confirmApproval() {
+    const url = `/collaboration-activities/${this.collaboration.id}/approvals`;
+    const response = yield fetch(url, {
+        method: 'POST',
+      }
+    ).catch(err => console.log(err));
+    if (response.status === 201) {
+      const url = `/collaboration-activities/${this.collaboration.id}`;
+      yield fetch(url, {
+          methor: 'PUT'
+        }
+      ).catch(err => console.log(err));
+    }
+    this.closeApprovalModal();
+    this.send('reloadModel');
+  }
+
+  @action
+  openApprovalModal() {
+    this.showApprovalModal = true;
+  }
+
+  @action
+  closeApprovalModal() {
+    this.showApprovalModal = false;
+  }
 }
--- app/routes/press-releases/press-release/shared/read.js
+++ app/routes/press-releases/press-release/shared/read.js
@@ -1,14 +1,20 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class PressReleasesPressReleaseSharedReadRoute extends Route {
   async afterModel(model) {
-    const collaboration = await model.collaboration;
-    this.collaborators = await collaboration.collaborators;
+    this.collaboration = await model.collaboration;
+    this.collaborators = await this.collaboration.collaborators;
   }
 
   setupController(controller) {
     super.setupController(...arguments);
+    controller.collaboration = this.collaboration;
     controller.collaborators = this.collaborators;
   }
 
+  @action
+  reloadModel() {
+    this.refresh();
+  }
 }
