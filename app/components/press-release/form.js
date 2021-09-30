24ee3a27c38b71adc6e9ff2fe84156bb66450fdc
Erika Pauwels <erika.pauwels@gmail.com>
2021-09-30 11:57:03 +0200

    Update html content setter

--- app/components/press-release/form.js
+++ app/components/press-release/form.js
@@ -34,7 +34,7 @@ export default class PressReleaseFormComponent extends Component {
 
   @action
   setHtmlContent(record, value) {
-    record["htmlContent"] = value;
+    record.htmlContent = value;
   }
 
   @action
