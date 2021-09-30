1f7a1485a074559bac2f328fa7458f623ce791a5
Erika Pauwels <erika.pauwels@gmail.com>
2021-09-30 14:14:51 +0200

    Cleanup collaborator validation for shared press-release

--- app/routes/press-releases/press-release/shared.js
+++ app/routes/press-releases/press-release/shared.js
@@ -4,23 +4,13 @@ import { inject as service } from '@ember/service';
 export default class PressReleasesPressReleaseSharedRoute extends Route {
   @service currentSession;
 
-  async model() {
-    const pressRelease = await this.modelFor('press-releases.press-release');
-    const collaboration = await pressRelease.collaboration;
-    const collaborators = await collaboration.collaborators
-    const loggedUser = this.currentSession.organization;
-    const index = collaborators.indexOf(loggedUser);
-    if (index > -1) {
-      return pressRelease;
-    }
-    return;
-  }
-
-  afterModel(model) {
-    if (model) {
-      this.transitionTo('press-releases.press-release.shared.read')
-    } else {
-      this.transitionTo('press-releases.overview.shared')
+  async afterModel(model) {
+    const collaboration = await model.collaboration;
+    const collaborators = await collaboration.collaborators;
+    const currentOrganization = this.currentSession.organization;
+    if (!collaborators.includes(currentOrganization)) {
+      // Organization of current user is not a collaborator
+      this.transitionTo('press-releases.overview.shared');
     }
   }
 }
