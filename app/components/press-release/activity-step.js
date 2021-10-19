35f30243007de708799d91e1fc0deb1ecff07fbb
Erika Pauwels <erika.pauwels@gmail.com>
2021-10-19 10:16:08 +0200

    Refactor historic activities timeline on shared press-release

--- app/components/press-release/activity-step.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import CONFIG from '../../config/constants';
-
-export default class PressReleaseActivityStepComponent extends Component {
-  constructor() {
-    super(...arguments);
-    this.CONFIG = CONFIG;
-  }
-}
--- app/models/press-release-activity.js
+++ app/models/press-release-activity.js
@@ -1,4 +1,7 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
+import CONFIG from '../config/constants';
+
+const ACTIVITY_TYPES = CONFIG.PRESS_RELEASE_ACTIVITY;
 
 export default class PressReleaseActivity extends Model {
   @attr('datetime') startDate;
@@ -7,4 +10,24 @@ export default class PressReleaseActivity extends Model {
   @belongsTo('organization') organization;
   @belongsTo('press-release') pressRelease;
   @belongsTo('user') creator;
+
+  get isCreationActivity() {
+    return this.type == ACTIVITY_TYPES.CREATE;
+  }
+
+  get isEditActivity() {
+    return this.type == ACTIVITY_TYPES.EDIT;
+  }
+
+  get isApprovalActivity() {
+    return this.type == ACTIVITY_TYPES.APPROVE;
+  }
+
+  get isSharingActivity() {
+    return this.type == ACTIVITY_TYPES.SHARE;
+  }
+
+  get isUnsharingActivity() {
+    return this.type == ACTIVITY_TYPES.UNSHARE;
+  }
 }
