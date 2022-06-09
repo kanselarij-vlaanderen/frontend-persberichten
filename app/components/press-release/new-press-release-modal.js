d699ef470d8c6868fdd5c6b7ca3fd287c76b4b5e
Erika Pauwels <erika.pauwels@gmail.com>
2022-06-09 10:56:27 +0200

    Wrap create new press release modal in a form with a submit button

--- app/components/press-release/new-press-release-modal.js
+++ app/components/press-release/new-press-release-modal.js
@@ -1,6 +1,13 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class PressReleaseNewPressReleaseModalComponent extends Component {
   @tracked title;
+
+  @action
+  createPressRelease(event) {
+    event.preventDefault();
+    this.args.onCreate(this.title);
+  }
 }
